b50e33d7e44c3cba82bfe9c4f0f9e96ec7257836
maycoldsm1234 <60947038+maycoldsm1234@users.noreply.github.com>
2020-08-02 16:31:48 -0500

    Add files via upload
    inicio Prueba

---
+++ index.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="en">
+  	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+		<meta name="author" content="Maycol David Sanchez Mora">
+		<meta name="generator" content="Jekyll v4.0.1">
+		<title>Prueba programacion - Sigma</title>
+
+		<link rel="canonical" href="https://localhost/prueba-sigma">
+
+		<!-- Bootstrap core CSS -->
+		
+		<link href="dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+		<link href="https://fonts.google.com/specimen/Roboto" rel="stylesheet">
+
+		<!-- Favicons -->
+		<link rel="apple-touch-icon" href="/docs/4.5/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+		<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+		<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+		<link rel="manifest" href="/docs/4.5/assets/img/favicons/manifest.json">
+		<link rel="mask-icon" href="/docs/4.5/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
+		<link rel="icon" href="/docs/4.5/assets/img/favicons/favicon.ico">
+		<meta name="msapplication-config" content="/docs/4.5/assets/img/favicons/browserconfig.xml">
+		<meta name="theme-color" content="#563d7c">
+
+
+		<!-- Custom styles for this template -->
+		<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+		<!--<link href="css/select2.min.css" rel="stylesheet">-->
+		<link href="css/select2-bootstrap.css" rel="stylesheet">
+		<link href="css/index.css" rel="stylesheet">
+		<link href="dist/form-validation.css" rel="stylesheet">
+		<script src="dist/sweet-alert/sweetalert.js"></script>
+    	<link rel="stylesheet" href="dist/sweet-alert/sweetalert.css">
+  	</head>
+	<body>
+  		<div class="container">
+  			<div class="py-5 text-center">
+    			<img class="d-block mx-auto mb-4" src="dist/img/sigma-logo.png" alt="" width="190" height="65">
+    			<h2>Prueba de desarrrollo sigma</h2>
+    			<p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+  			</div>
+
+			<div class="row">
+    			<div class="col-md-6 order-md-1">
+     				<img class="d-block mx-auto" src="dist/img/sigma-image.png" alt="" width="100%">
+    			</div>
+    			<div class="col-md-6 order-md-2">
+      				<form class="needs-validation" method="post" actsion="">
+						<div class="row">
+							<div class="col-md-12 mb-6">
+								<label for="firstName"><strong>Departamento<span class="text-muted">*</span></strong></label>
+								<select class="form-control" id="departamento" name="departamento" placeholder=""  required>
+									<option selected disabled value="">Seleccione un departamento</option>
+								</select>
+						  	</div>
+						</div>
+						<div class="row">
+						  	<div class="col-md-12 mb-6">
+								<label for="firstName"><strong>Ciudad<span class="text-muted">*</span></strong></label>
+								<select class="form-control" id="ciudad" name="ciudad" placeholder="" required>
+									<option selected disabled value="">Seleccione una ciudad</option>
+								</select>
+						  	</div>
+						</div>
+						<div class="row">
+						  	<div class="col-md-12 mb-6">
+								<label for="firstName"><strong>Nombre<span class="text-muted">*</span></strong></label>
+								<input type="text" class="form-control" id="nombre" name="nombre" placeholder="" maxlength="50" value="" required>
+						  	</div>
+						</div>
+						<div class="row">
+						  	<div class="col-md-12 mb-6">
+								<label for="firstName"><strong>Correo<span class="text-muted">*</span></strong></label>
+								<input type="email" class="form-control" id="email" name="email" placeholder="you@example.com" value="" required>
+						  	</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12 mb-6 d-flex justify-content-center">
+        						<button class="btn btn-primary btn-md btn-block" type="submit"><strong>ENVIAR</strong></button>
+							</div>
+						</div>
+      				</form>
+				</div>
+  			</div>
+			
+  			<footer class="my-5 pt-5 text-muted text-center text-small">
+    			<p class="mb-1">&copy; 2020 Maycol David Sanchez Mora</p>
+  			</footer>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+      	<!--<script>window.jQuery || document.write('<script src="dist/js/jquery.slim.min.js"><\/script>')</script><script src="dist/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>-->
+		<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+		<!--<script type="application/javascript" src="js/select2.min.js" ></script>-->
+        <script src="dist/form-validation.js"></script>
+		<script type="application/javascript" src="dist/js/jquery.validate.js"></script>
+		<script type="application/javascript" src="js/index.js" ></script>
+	</body>
+</html>
